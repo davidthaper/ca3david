33b1a2cbefbf93cba34476d9a1e68ec7c363e688
davidthaper <49223258+davidthaper@users.noreply.github.com>
2019-04-15 23:25:43 +0530

    Add files via upload

--- david.c
+++ david.c
@@ -9,18 +9,40 @@ algorithm and calculate the average turnaround time and waiting time.
 #include<stdio.h>
 int main()
 {
-    int arr_t[100],burst_t[100],priority[100];
+    int arrival_time[50],burst_time[50],remain_time[50],complete_time,left=0;
+    int waiting_time[50],TurnArroundTime[50];
     int i,n;
     printf("Enter no of Processes : ");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         printf("Enter arrival time for process P%d : ",i+1);
-        scanf("%d",&arr_t[i]);
+        scanf("%d",&arrival_time[i]);
         printf("Enter burst time for process P%d : ",i+1);
-        scanf("%d",&burst_t[i]);
-        printf("Enter priority for process P%d :",i+1);
-        scanf("%d",&priority[i]);
+        scanf("%d",&burst_time[i]);
+    
+        remain_time[i]=burst_time[i];
     }
+int small,time;
+	float total_wait = 0,total_turn_around = 0;
+	remain_time[19]=0;
+	for(time = 0;left != n;time++){
+		small = 19;
+		for(i=0;i<n;i++){
+			if(arrival_time[i]<=time && burst_time[i]>remain_time[small] && remain_time[i]>0){
+				small = i;
+			}
+		}
+		remain_time[small]--;
+		if(remain_time[small]==0){
+			left++;
+			complete_time = time++;
+			total_turn_around += complete_time - arrival_time[small]; 
+			total_wait += complete_time - arrival_time[small] - burst_time[small];
+			
+		}
+	}
+	
+	printf("the total turn around time is: %2f \n",total_turn_around/n);
+	printf("the total waiting time is: %2f",total_wait/n);
 }
-
